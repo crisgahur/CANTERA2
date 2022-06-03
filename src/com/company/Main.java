ef18e0d1b64279710731f8450e5067e383e8a0da
crisgahur <90589221+crisgahur@users.noreply.github.com>
2022-06-02 19:15:24 -0500

    Add files via upload
    Creating new methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
  * Main class,
  *
  *
- * @version 1.01.001 2022-06-01.
+ * @version 1.05.003 2022-06-01.
  *
  * @author Cristian Gallego Hurtado - cris.gahur@hotmail.com
  * @author Juan Pablo Tabares Gallego - cris.gahur@hotmail.com
@@ -18,34 +18,60 @@ public class Main {
     static Scanner getString = new Scanner(System.in);
     static Scanner getInt = new Scanner(System.in);
     static public int option;
-    static public int option2;
     static public String option3;
     public static Songs[] allSongs = new Songs[5];
     public static Songs[] playList = new Songs[3];
 
     public static void main(String[] args) {
 
-        Songs song1 = new Songs("GASOLINA", 2,"20-03-2002","Reggaeton music to dance in parties",3,"reggaeton","/path/img/cover1.jpg" );
-        Songs song2 = new Songs("EL CANTANTE", 2,"12-10-1987","Salsa music to enjoy",3,"salsa","/path/img/cover2.jpg");
-        Songs song3 = new Songs("BEAT IT", 2,"23-07-1995","Pop music to dance with friends",3,"pop","/path/img/cover3.jpg");
-        Songs song4 = new Songs("YOUR LOVE", 2,"20-03-2002","Rock music to enjoy with friends",3,"rock","/path/img/cover4.jpg");
-        Songs song5 = new Songs("PLASTICO", 2,"06-12-1997","Salsa music to enjoy.",3,"salsa","/path/img/cover5.jpg");
+        Songs song1 = new Songs("GASOLINA", 2,20,2,2002,"Latin music" , 5 , "reggaeton" , "/path/img/cover1.jpg" );
+        Songs song2 = new Songs("EL CANTANTE", 2,12,2,1957,"Salsa music to enjoy",4,"salsa", "/path/img/cover2.jpg");
+        Songs song3 = new Songs("BEAT IT", 2,2,10,1969,"Pop music to dance with friends",3,"pop","/path/img/cover3.jpg");
+        Songs song4 = new Songs("YOUR LOVE", 2,20,3,1960,"Rock music to enjoy with friends",3,"rock", "/path/img/cover4.jpg");
+        Songs song5 = new Songs("PLASTICO", 2,6,12,1947,"Salsa music to enjoy.", 6,"salsa","/path/img/cover5.jpg");
 
         List<Songs> list= Arrays.asList(song1, song2, song3, song4, song5);
         allSongs[0] = song1;
 
 
-        switch (3){
+        System.out.println("Welcome to the app music, please choose what you would to do: ");
+        System.out.println("1. Show me the main library");
+        System.out.println("2. Create a new playlist");
+        System.out.println("3. Show me a playlist");
+        System.out.println("4. Desconectarse");
+        option = getInt.nextInt();
+        switch (option){
             case 1 :
                 //MAIN LIBRARY
-                songsList("1. ", song1.name, song1.duration, song1.genre, song1.date);
-                songsList("2. ", song2.name, song2.duration, song2.genre, song2.date);
-                songsList("3. ", song3.name, song3.duration, song3.genre, song3.date);
-                songsList("4. ", song4.name, song4.duration, song4.genre, song4.date);
-                songsList("5. ", song5.name, song5.duration, song5.genre, song5.date);
+                songsList("1. ", song1.getName(), song1.getDuration(), song1.genre, song1.date);
+                songsList("2. ", song2.getName(), song2.getDuration(), song2.genre, song2.date);
+                songsList("3. ", song3.getName(), song3.getDuration(), song3.genre, song3.date);
+                songsList("4. ", song4.getName(), song4.getDuration(), song4.genre, song4.date);
+                songsList("5. ", song5.getName(), song5.getDuration(), song5.genre, song5.date);
 
-                break;
+                System.out.println("CHOOSE WHAT DO YOU WOULD TO FILTER:");
+                System.out.println("1. GENRE");
+                System.out.println("2. YEAR");
+                option = getInt.nextInt();
 
+                switch (option){
+                    case 1:
+                System.out.println("WRITE THE GENRE:");
+                option3 = getString.next();
+
+                ArrayList<String> filteredResult = new ArrayList<>();
+
+                for (Songs songs: list) {
+                    if (songs.genre.equals(option3)) {
+
+                        filteredResult.add("Name: " + songs.getName() + "\n" + "Duration: " + songs.getDuration() + " Min " +  "\n" + "Song year: " + songs.date + "\n \n" );
+                    }
+                }
+
+                        System.out.println("Filtered Result:\n"+ filteredResult.toString());
+
+                }
+                 break;
             case 2 :
                 //CREATE LIBRARY OPTION
                 System.out.println("WRITE YOURS LIBRARY'S NAME");
@@ -53,34 +79,13 @@ public class Main {
 
                 for(int i = 0; i <= 3; i++){
                     if(playList[i] == null){
-                        playList[i] = allSongs[chosenSong(song1.name, song2.name, song3.name, song4.name, song5.name)-1];
+                        playList[i] = allSongs[chosenSong(song1.getName(), song2.getName(), song3.getName(), song4.getName(), song5.getName())-1];
                     }
                 }
                 break;
 
             case 3:
-                // FILTER
-                System.out.println("CHOOSE WHAT DO YOU WOULD TO FILTER:");
-                System.out.println("1. GENRE");
-                System.out.println("2. YEAR");
-                option2 = getInt.nextInt();
 
-                switch (option2){
-                    case 1:
-                        System.out.println("WRITE THE GENRE:");
-                        option3 = getString.next();
-
-                        ArrayList<String> filteredResult = new ArrayList<>();
-
-                        for (Songs songs: list) {
-                            if (songs.genre.equals(option3)) {
-
-                                filteredResult.add("Name: " + songs.name + "\n" + "Duration: " + songs.duration + "\n" + "Song year: " + songs.date + "\n \n" );
-                            }
-                        }
-
-                        System.out.println("Filtered Result:\n"+filteredResult.toString());
-                }
                 }
 
 
@@ -89,7 +94,7 @@ public class Main {
 
 
 
-    private static void songsList(String num,String name , int time, String type, String day){
+    private static void songsList(String num,String name , float time, String type, String day){
         System.out.println(num + name + " \n  Duration: " + time + "  Min" + " \n  Genre: " + type + " \n  Date " + day);
     }
 
--- src/com/company/Songs.java
+++ src/com/company/Songs.java
@@ -16,28 +16,92 @@ public class Songs {
     /**
      * Declaration of attributes.
      */
-    public String name;
-    public int id;
-    public String date;
-    public String description;
-    public int duration;
+    private String name;
+    private int id;
+    private int day;
+    private int month;
+    private int year;
+    private String description;
+    private float duration;
     public String genre;
-    public String cover;
+    private String cover;
+    public String date;
 
-    public Songs(String name, int id, String date, String description, int duration, String genre, String cover) {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public Songs(String name, int id, int day, int month, int year, String description, int duration, String genre, String cover) {
         this.name = name;
         this.id = id;
-        this.date = date;
+        this.date = day + "/" + month + "/" + year;
+        this.day = day;
+        this.month = month;
+        this.year = year;
         this.description = description;
         this.duration = duration;
         this.genre = genre;
         this.cover = cover;
     }
-
-    public void filterGenre(){
-
-    };
-    public void filterDate(){
-
-    };
 }
